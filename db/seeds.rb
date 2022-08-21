9039cde56cd41f2a603953f0c6951b45c44675a7
sonali <sonalipatle8@gmail.com>
2022-08-21 13:26:15 +0530

    User page added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  User.create({
+    first_name: "sonali",
+    last_name: "patle",
+    email: "spatle8@gmail.com",
+    password: "5852564"
+  })
+  User.create({
+    first_name: "ravi",
+    last_name: "baghele",
+    email: "rb8@gmail.com",
+    password: "585254451"
+  })
